befe20ae389fc827c73f7438c31f887883873816
Serephim-hash <tedd61945@eleve.isep.fr>
2025-06-17 11:52:29 +0200

    erreur sur Capteur.php, getter find all

--- app/Model/Entity/Capteur.php
+++ app/Model/Entity/Capteur.php
@@ -5,10 +5,9 @@ Class Capteur {
     private ?float $temperature;
     private ?int $segment_7;
     private ?bool $lumiere_photo;
-    private ?array $information;
     const Table_name = "Infos_capteurs";
 // Getters
     public function getId_Capteur(): ?int { return $this->id_capteur; }
     public function getId_Manege(): ?int { return $this->id_manege; }
-    public function getAll(): ?array {return $this->$information;}
+    public function getAll(): ?array {return $this->array=[$segment_7, $temperature, $lumiere_photo ];}
 }
