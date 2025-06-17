3f02d35db773ea0fd55b86051eb5978088b0ec43
Serephim-hash <tedd61945@eleve.isep.fr>
2025-06-17 14:41:11 +0200

    capteur modif

--- app/Model/Entity/Capteur.php
+++ app/Model/Entity/Capteur.php
@@ -2,9 +2,9 @@
 abstract Class Capteur {
     protected ?int $id_capteur;
     protected ?int $id_manege;
-    protected ?float $informations;
+    protected ?string $informations;
 // Getters
     public function getId_Capteur(): ?int { return $this->id_capteur; }
     public function getId_Manege(): ?int { return $this->id_manege; }
-    public function getInformation(): ?float {return $this->informations; }
+    public function getInformation(): ?string {return $this->informations; }
 }
